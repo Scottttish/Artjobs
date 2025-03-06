431571150771dc5a0270474eded89836fbd057c3
Scottttish <scotthouston314@gmail.com>
2025-03-06 23:33:44 +0500

    Создание App.test.js

---
+++ App.test.js
@@ -0,0 +1,54 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from './App';
+
+test('renders Рандомайзер header', () => {
+  render(<App />);
+  const headerElement = screen.getByText(/Рандомайзер/i);
+  expect(headerElement).toBeInTheDocument();
+});
+
+test('renders input fields and button', () => {
+  render(<App />);
+  
+  const firstInput = screen.getByPlaceholderText(/Введите первое число/i);
+  const secondInput = screen.getByPlaceholderText(/Введите второе число/i);
+  const button = screen.getByText(/Нажми/i);
+  
+  expect(firstInput).toBeInTheDocument();
+  expect(secondInput).toBeInTheDocument();
+  expect(button).toBeInTheDocument();
+});
+
+test('generates random number', () => {
+  render(<App />);
+  
+  const firstInput = screen.getByPlaceholderText(/Введите первое число/i);
+  const secondInput = screen.getByPlaceholderText(/Введите второе число/i);
+  const button = screen.getByText(/Нажми/i);
+
+  fireEvent.change(firstInput, { target: { value: '10' } });
+  fireEvent.change(secondInput, { target: { value: '5' } });
+
+  fireEvent.click(button);
+
+  const randomLabel = screen.getByClassName('rnd_number');
+  expect(randomLabel).toBeInTheDocument();
+  expect(parseInt(randomLabel.textContent)).toBeGreaterThanOrEqual(5);
+  expect(parseInt(randomLabel.textContent)).toBeLessThanOrEqual(10);
+});
+
+test('alerts when invalid input is entered', () => {
+  render(<App />);
+  
+  const firstInput = screen.getByPlaceholderText(/Введите первое число/i);
+  const secondInput = screen.getByPlaceholderText(/Введите второе число/i);
+  const button = screen.getByText(/Нажми/i);
+
+  fireEvent.change(firstInput, { target: { value: '5' } });
+  fireEvent.change(secondInput, { target: { value: '10' } });
+
+  fireEvent.click(button);
+
+  // Проверяем, что alert был вызван
+  expect(window.alert).toHaveBeenCalledWith("Введите правильные числа и первое число должно быть больше второго, а также оба числа не должны быть равны");
+});

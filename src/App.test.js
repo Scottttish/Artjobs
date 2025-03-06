2daa4f94424053c5529235219cf62540bdfb6a4e
Scottttish <scotthouston314@gmail.com>
2025-03-06 23:39:20 +0500

    Изменить App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,53 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Рандомайзер header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/Рандомайзер/i);
+  expect(headerElement).toBeInTheDocument();
+});
+
+test('renders input fields and button', () => {
+  render(<App />);
+  
+  const firstInput = screen.getByPlaceholderText(/Введите первое число/i);
+  const secondInput = screen.getByPlaceholderText(/Введите второе число/i);
+  const button = screen.getByText(/Нажми/i);
+  
+  expect(firstInput).toBeInTheDocument();
+  expect(secondInput).toBeInTheDocument();
+  expect(button).toBeInTheDocument();
+});
+
+test('generates random number', () => {
+  render(<App />);
+  
+  const firstInput = screen.getByPlaceholderText(/Введите первое число/i);
+  const secondInput = screen.getByPlaceholderText(/Введите второе число/i);
+  const button = screen.getByText(/Нажми/i);
+
+  fireEvent.change(firstInput, { target: { value: '10' } });
+  fireEvent.change(secondInput, { target: { value: '5' } });
+
+  fireEvent.click(button);
+
+  const randomLabel = screen.getByClassName('rnd_number');
+  expect(randomLabel).toBeInTheDocument();
+  expect(parseInt(randomLabel.textContent)).toBeGreaterThanOrEqual(5);
+  expect(parseInt(randomLabel.textContent)).toBeLessThanOrEqual(10);
+});
+
+test('alerts when invalid input is entered', () => {
+  render(<App />);
+  
+  const firstInput = screen.getByPlaceholderText(/Введите первое число/i);
+  const secondInput = screen.getByPlaceholderText(/Введите второе число/i);
+  const button = screen.getByText(/Нажми/i);
+
+  fireEvent.change(firstInput, { target: { value: '5' } });
+  fireEvent.change(secondInput, { target: { value: '10' } });
+
+  fireEvent.click(button);
+
+  expect(window.alert).toHaveBeenCalledWith("Введите правильные числа и первое число должно быть больше второго, а также оба числа не должны быть равны");
 });

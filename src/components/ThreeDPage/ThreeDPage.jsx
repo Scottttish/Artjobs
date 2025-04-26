9acccde8127ed99ddfb2c415fe046b75aa6782ee
Scottttish <scotthouston314@gmail.com>
2025-04-27 01:53:07 +0500

    Update ThreeDPage.jsx

--- src/components/ThreeDPage/ThreeDPage.jsx
+++ src/components/ThreeDPage/ThreeDPage.jsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js';
 import './3DPage.css';
 
-// Инициализация Supabase клиента
+// Инициализация Supabase клиента с новым ключом
 const supabase = createClient(
   'https://jvccejerkjfnkwtqumcd.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2Y2NlamVya2pmbmt3dHF1bWNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1MTMzMjAsImV4cCI6MjA2MTA4OTMyMH0.xgqIMs3r007pJIeV5P8y8kG4hRcFqrgXvkkdavRtVIw'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2Y2NlamVya2pmbmt3dHF1bWNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1MTMzMjAsImV4cCI6MjA2MTA4OTMyMH0.xgqIMs3r007pJIeV5P8y8kG4hRcFqrgXvkkdavRtVIw'
 );
 
 function ThreeDPage() {

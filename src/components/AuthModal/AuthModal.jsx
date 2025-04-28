2f5f533bc0ad5d32b915f68c028a54be34591369
Scottttish <scotthouston314@gmail.com>
2025-04-29 04:10:31 +0500

    Update AuthModal.jsx

--- src/components/AuthModal/AuthModal.jsx
+++ src/components/AuthModal/AuthModal.jsx
@@ -3,10 +3,8 @@ import { createClient } from '@supabase/supabase-js';
 import './AuthModal.css';
 import mainImage from '../../assets/main.jpg';
 
-// Функция для создания задержки
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-// Создание клиента Supabase (рекомендуется вынести в supabase.js)
 const supabase = createClient(
   'https://jvccejerkjfnkwtqumcd.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2Y2NlamVya2pmbmt3dHF1bWNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1MTMzMjAsImV4cCI6MjA2MTA4OTMyMH0.xgqIMs3r007pJIeV5P8y8kG4hRcFqrgXvkkdavRtVIw'

f2f7814994977314f026997569ef44a2762ada84
Scottttish <scotthouston314@gmail.com>
2025-05-01 03:51:36 +0500

    Update HeroSection.test.jsx

--- src/components/HeroSection/HeroSection.test.jsx
+++ src/components/HeroSection/HeroSection.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import HeroSection from './HeroSection';
+import HeroSection from './HeroSection.jsx';
 import Slider from 'react-slick';
 
 jest.mock('react-slick', () => {

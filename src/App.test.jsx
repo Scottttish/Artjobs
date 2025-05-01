ae20ff3b1c0fe72f48a53df897c0ce585cbdf898
Scottttish <scotthouston314@gmail.com>
2025-05-01 12:03:34 +0500

    Update App.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,14 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-beforeAll(() => {
-  window.matchMedia = window.matchMedia || function() {
-    return {
-      matches: false,
-      addListener: jest.fn(),
-      removeListener: jest.fn(),
-    };
-  };
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
 });
 
 describe('Компонент App', () => {

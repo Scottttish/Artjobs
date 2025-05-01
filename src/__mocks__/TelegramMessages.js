dfd8b65eb7bfbce9b107de4fdf8d69d64df220b0
Scottttish <scotthouston314@gmail.com>
2025-05-01 11:28:33 +0500

    Update TelegramMessages.js

--- src/__mocks__/TelegramMessages.js
+++ src/__mocks__/TelegramMessages.js
@@ -1,1 +1,19 @@
-export const sendToTelegram = jest.fn(() => Promise.resolve());
+export const sendToTelegram = async (name, email, message) => {
+  const response = await fetch('https://api.telegram.org/bot<YOUR_BOT_API>/sendMessage', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      chat_id: '<YOUR_CHAT_ID>',
+      text: `New feedback received: 
+      Name: ${name} 
+      Email: ${email} 
+      Message: ${message}`,
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error('Error sending message to Telegram');
+  }
+};

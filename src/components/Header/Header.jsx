137230c008544ef6567b04a72055c1d15a18129f
Scottttish <scotthouston314@gmail.com>
2025-05-04 18:04:01 +0500

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import { useState, useEffect } from 'react';
 import { NavLink, useNavigate, useLocation } from 'react-router-dom';
-import { supabase } from '../supabaseClient';
+import { createClient } from '@supabase/supabase-js';
 import './Header.css';
 import logo from '../../assets/logo.png';
 import icon from '../../assets/icon.png';
 import AuthModal from '../AuthModal/AuthModal';
+
+const supabaseUrl = 'https://jvccejerkjfnkwtqumcd.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2Y2NlamVya2pmbmt3dHF1bWNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1MTMzMjAsImV4cCI6MjA2MTA4OTMyMH0.xgqIMs3r007pJIeV5P8y8kG4hRcFqrgXvkkdavRtVIw';
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 function Header() {
   const [showAuthModal, setShowAuthModal] = useState(false);
